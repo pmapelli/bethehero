b82fad4a822f3c4f0161d84b19ebc30324677aed
pmapelli <pedromapelli@MacBook-Air-de-Pedro.local>
2020-04-01 13:25:21 -0300

    feat: remove .env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
-
+// const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+const api = axios.create({
+  baseURL: "https://apple-tart-67179.herokuapp.com/"
+});
 export default api;
